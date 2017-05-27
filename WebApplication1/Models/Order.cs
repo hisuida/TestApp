cf5e83c05c01c0546e29fb3f4bef20c0ae87d1f9
hisuida <hisuida@me.com>
2017-05-27 13:24:54 -0400

    Added validation and footer

--- WebApplication1/Models/Order.cs
+++ WebApplication1/Models/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,13 @@ namespace WebApplication1.Models
 {
     public class Order
     {
+        [Required(ErrorMessage = "Please enter a Serial Number")]
+        [MaxLength(7, ErrorMessage = "Please enter a 7 digit Serial Number")]
+        [MinLength(7, ErrorMessage = "Please enter a 7 digit Serial Number")]
         public string SerialNumber_ID { get; set; }
+
         public string CustomerID { get; set; }
+        public string DisplayID { get; set; }
         public DateTime OrderDate { get; set; }
         public string OrderNumber { get; set; }
         public string SiteName { get; set; }
